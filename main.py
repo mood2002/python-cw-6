7798c56fa89191723710b9967ffce1b8c486f7c0
mood2002 <142811282+mood2002@users.noreply.github.com>
2023-09-06 19:25:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # write your code here
+person = { 
+   "name": "mariam",
+    "age": 16   ,
+     "hobbies": ["Drawing","reading","gaming"]
+}
+
+print(person["name"])
+print(person["age"])
+
+person["age"] = 14
+person["country"] = "Egypt"
+person["hobbies"].append("Writing")
+print(person)
+print(len(person))
+
+def check_hobbies(person):
+    if len(person["hobbies"]) > 3:
+        print("WOW YOU ARE AMAZING")
+check_hobbies(person)
